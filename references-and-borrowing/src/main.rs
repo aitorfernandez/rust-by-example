c0697558f8d7c63a08d5f6a423baa95a6b009d3a
Aitor Fernandez <aitor.fernandez@truelayer.com>
2021-11-11 13:34:11 +0000

    add dangling and mutable references

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -13,6 +13,56 @@ fn main() {
     println!("{}", s);
 
     // One mutable reference to a particular piece of data
+    let mut s = String::from("Hello");
+
+    // error two mutable borrow
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+
+    // println!("{}, {}", r1, r2);
+
+    // ok
+    {
+        let r1 = &mut s;
+    }
+
+    let r2 = &mut s;
+
+    // Don't combine mutable and inmutable references.
+    // error
+    // let mut s = String::from("Hello");
+
+    // let r1 = &s;
+    // let r2 = &s;
+    // let r3 = &mut s;
+
+    // println!("{}, {}, {}", r1, r2, r3);
+
+    // ok
+    let mut s = String::from("Hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{}, {}", r1, r2);
+
+    let r3 = &mut s;
+
+    println!("{}", r3);
+
+    // Dangling references
+    let reference_to_nothing = dangle();
+}
+
+// fn dangle() -> &String {
+//     let s = String::from("Hello");
+//     &s
+//     // s goes outh of scope
+// }
+
+fn dangle() -> String {
+    let s = String::from("Hello");
+    s
+    // Ownership is moved out
 }
 
 // s is a reference but no ownership
