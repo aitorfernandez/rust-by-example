64498288fb616094b7a47ae512810719a1f1da77
Aitor Fernandez <helloimaitor@gmail.com>
2021-11-15 09:18:14 +0000

    crates and modules

---
+++ crates-and-modules/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod english;
+
+// These two declarations tell Rust to look for either src/english.rs and src/japanese.rs, or src/english/mod.rs and src/japanese/mod.rs, depending on our preference
+
+pub mod japanese;
+
+// mod english {
+//     mod greetings {}
+
+//     mod farewells {}
+// }
+
+// mod japanese {
+//     mod greetings {}
+
+//     mod farewells {}
+// }
