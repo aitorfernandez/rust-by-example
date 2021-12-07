859a2e70af251bacec5c5804f63368b1c59bf3be
Aitor Fernandez <helloimaitor@gmail.com>
2021-12-07 20:47:01 +0000

    add configuration and update test for database connection

--- email-newsletter/src/startup.rs
+++ email-newsletter/src/startup.rs
@@ -1,15 +1,19 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let pool = web::Data::new(pool);
     // HttpServer handles all transport level concerns.
-    let server = HttpServer::new(|| {
+    let server = HttpServer::new(move || {
         // App is where all application logic lives, routing, middlewares, request...
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(pool.clone())
     })
     .listen(listener)?
     .run();
--- email-newsletter/tests/health_check.rs
+++ email-newsletter/tests/health_check.rs
@@ -1,15 +1,23 @@
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
+
+pub struct TestApp {
+    pub address: String,
+    pub pool: PgPool,
+}
 
 #[actix_rt::test]
 async fn health_check_test() {
     // Arrange
-    let address = spawn_app();
+    let app = spawn_app().await;
     // perform HTTP requests against our application
     let client = reqwest::Client::new();
 
     // Act
     let res = client
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -22,13 +30,13 @@ async fn health_check_test() {
 #[actix_rt::test]
 async fn subscribe_returns_200_test() {
     // Arrange
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let body = "name=foo%20bar&email=foo_bar%40gmail.com";
 
     // Act
     let res = client
-        .post(&format!("{}/subscriptions", &address))
+        .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -37,12 +45,20 @@ async fn subscribe_returns_200_test() {
 
     // Assert
     assert_eq!(200, res.status().as_u16());
+
+    let saved = sqlx::query!("select email, name from subscription")
+        .fetch_one(&app.pool)
+        .await
+        .expect("Failed to fetch saved subscriptions");
+
+    assert_eq!(saved.email, "foo_bar@gmail.com");
+    assert_eq!(saved.name, "foo bar");
 }
 
 #[actix_rt::test]
 async fn subscribe_returns_400_test() {
     // Arrange
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let tests = vec![
         ("name=foo%20bar", "missing the email"),
@@ -53,7 +69,7 @@ async fn subscribe_returns_400_test() {
     for (invalid_body, error_message) in tests {
         // Act
         let res = client
-            .post(&format!("{}/subscriptions", &address))
+            .post(&format!("{}/subscriptions", &app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
@@ -69,10 +85,21 @@ async fn subscribe_returns_400_test() {
     }
 }
 
-fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = email_newsletter::startup::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    let server = run(listener, pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+
+    TestApp {
+        address,
+        pool: pool,
+    }
 }
